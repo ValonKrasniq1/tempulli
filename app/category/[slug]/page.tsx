32f48f59505ded8e8b7cfe7f5e5c15b02ac9cb6a
Valon Krasniqi <valonkras98@gmail.com>
2026-06-21 20:39:12 +0200

    Add category and subcategory pages

---
+++ app/category/[slug]/page.tsx
@@ -0,0 +1,9 @@
+export default function CategoryPage() {
+  return (
+    <div className="p-10">
+      <h1 className="text-4xl font-bold">
+        Faqja e kategorisë
+      </h1>
+    </div>
+  );
+}
---
+++ app/subcategory/[slug]/page.tsx
@@ -0,0 +1,9 @@
+export default function SubcategoryPage() {
+  return (
+    <div className="p-10">
+      <h1 className="text-4xl font-bold">
+        Faqja e nënkategorisë
+      </h1>
+    </div>
+  );
+}
