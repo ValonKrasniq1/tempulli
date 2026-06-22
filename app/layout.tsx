c302f4809f9a2eeb0f716a735d910ea072fd6438
Valon Krasniqi <valonkras98@gmail.com>
2026-06-22 19:30:24 +0200

    Add Google Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import TrackPageView from "../components/TrackPageView";
 
@@ -29,6 +30,20 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-KX926FTN0S"
+          strategy="afterInteractive"
+        />
+
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-KX926FTN0S');
+          `}
+        </Script>
+
         <TrackPageView />
         {children}
       </body>
